2f8e6fe6089080292c0bdd671c27ccfe8217ca6c
rohit6969 <41963892+rohit6969@users.noreply.github.com>
2021-09-22 14:11:31 +0545

    Add files via upload

---
+++ Meeting Schedular/ConsoleApp1/ConsoleApp1/Invitation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Invitation
+    {
+        public Guid Id { get; set; }
+        public Status Status { get; set; }
+        public DateTime SentAt { get; set; }
+        public DateTime RepliedAt { get; set; }
+        public User Invitee { get; set; }
+        public Meeting Meeting { get; set; }
+    }
+
+    public enum Status
+    {
+        Accepted,
+        Rejected,
+        Undecided
+    }
+}
---
+++ Meeting Schedular/ConsoleApp1/ConsoleApp1/Meeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Meeting
+    {
+        public Guid Id { get; set; }
+        public string Details { get; set; }
+        public DateTime MeetingTime { get; set; }
+        public int Duration { get; set; }
+        public int MinAttendees { get; set; }
+        public int MaxAttendees { get; set; }
+    }
+}
